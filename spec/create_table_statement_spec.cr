7a2b0e03831ad0cbfa88f7d2ac72bb9e65e733da
mikeeus <itsmikias@gmail.com>
2017-11-15 09:06:58 +0300

    Separate index specs, add spec for non btree indexes

--- spec/create_table_statement_spec.cr
+++ spec/create_table_statement_spec.cr
@@ -3,12 +3,12 @@ require "./spec_helper"
 describe LuckyMigrator::CreateTableStatement do
   it "can create tables" do
     statement = LuckyMigrator::CreateTableStatement.new(:users).build do
-      add name : String, index: true
+      add name : String
       add age : Int32
       add completed : Bool
       add joined_at : Time
       add amount_paid : Float
-      add email : String?, index: true, unique: true
+      add email : String?
     end
 
     statement.should eq <<-SQL
@@ -22,8 +22,33 @@ describe LuckyMigrator::CreateTableStatement do
       joined_at timestamp NOT NULL,
       amount_paid decimal NOT NULL,
       email text)
+    SQL
+  end
+
+  it "can create tables with indexes" do
+    statement = LuckyMigrator::CreateTableStatement.new(:users).build do
+      add name : String, index: true
+      add email : String, index: true, unique: true
+    end
+
+    statement.should eq <<-SQL
+    CREATE TABLE users (
+      id serial PRIMARY KEY,
+      created_at timestamp NOT NULL,
+      updated_at timestamp NOT NULL,
+      name text NOT NULL,
+      email text NOT NULL)
     CREATE INDEX users_name_index ON users USING btree (name);
     CREATE UNIQUE INDEX users_email_index ON users USING btree (email);
     SQL
+  end
+
+  it "raises error on columns with non btree indexes" do
+    expect_raises Exception, "index type 'gist' not supported" do
+      LuckyMigrator::CreateTableStatement.new(:users).build do
+        add name : String, index: true
+        add email : String, index: true, using: "gist"
+      end
+    end
   end
 end
