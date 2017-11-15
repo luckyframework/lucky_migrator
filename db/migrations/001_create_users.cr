244571817c117bb252e74f371884b591f39c6e81
mikeeus <itsmikias@gmail.com>
2017-11-15 21:27:48 +0300

    Add index

--- db/migrations/001_create_users.cr
+++ db/migrations/001_create_users.cr
@@ -1,7 +1,7 @@
 class CreateUsers::V001 < LuckyMigrator::Migration::V1
   def migrate
     create :users do
-      add first_name : String
+      add first_name : String, index: true
     end
   end
 
