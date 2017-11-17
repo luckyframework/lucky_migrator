6742479be1929e75b24d0a21d1113e4239fe5ad5
mikeeus <itsmikias@gmail.com>
2017-11-17 13:49:31 +0300

    Add add_index helper method. Add indexes to migration

--- db/migrations/001_create_users.cr
+++ db/migrations/001_create_users.cr
@@ -2,7 +2,10 @@ class CreateUsers::V001 < LuckyMigrator::Migration::V1
   def migrate
     create :users do
       add first_name : String, index: true
+      add last_name : String
     end
+
+    add_index :users, :last_name, unique: true
   end
 
   def rollback
