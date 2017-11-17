31e9a5a05baf4b8d0020bd85a169460f4541220a
mikeeus <itsmikias@gmail.com>
2017-11-17 17:40:02 +0300

    Add default value for using, update specs

--- src/lucky_migrator/create_index_statement.cr
+++ src/lucky_migrator/create_index_statement.cr
@@ -10,7 +10,8 @@
 class LuckyMigrator::CreateIndexStatement
   ALLOWED_INDEX_TYPES = %w[btree]
 
-  def initialize(@table : Symbol, @column : String | Symbol, @using : String | Symbol, @unique = false)
+  def initialize(@table : Symbol, @column : String | Symbol, @using : String | Symbol = :btree, @unique = false)
+
     raise "index type '#{using}' not supported" unless ALLOWED_INDEX_TYPES.includes?(using.to_s)
   end
 
