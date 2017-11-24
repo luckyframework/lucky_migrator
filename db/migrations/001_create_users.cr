41bec5f5607000c810ac44731ef179510154985e
mikeeus <itsmikias@gmail.com>
2017-11-24 11:26:02 +0300

    Add Float options to migration

--- db/migrations/001_create_users.cr
+++ db/migrations/001_create_users.cr
@@ -3,6 +3,7 @@ class CreateUsers::V001 < LuckyMigrator::Migration::V1
     create :users do
       add first_name : String, index: true
       add last_name : String
+      add salary : Float, precision: 10, scale: 2
     end
 
     create_index :users, :last_name, unique: true
