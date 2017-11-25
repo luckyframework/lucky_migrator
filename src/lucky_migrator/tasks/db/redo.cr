47a608bd1704bb149f14f9f5189b6df6ec11bfca
mikeeus <itsmikias@gmail.com>
2017-11-25 07:00:11 +0300

    Remove unnecessary begin/rescue

--- src/lucky_migrator/tasks/db/redo.cr
+++ src/lucky_migrator/tasks/db/redo.cr
@@ -4,11 +4,7 @@ class Db::Redo < LuckyCli::Task
   banner "Rollback then run the last migration"
 
   def call
-    begin
-      Db::Rollback.new.call
-      Db::Migrate.new.call
-    rescue e : PQ::PQError
-      puts e.message.colorize(:red)
-    end
+    Db::Rollback.new.call
+    Db::Migrate.new.call
   end
 end
