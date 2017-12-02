90c58d4170659a72462523c94aeb0e8404a695bd
mikeeus <itsmikias@gmail.com>
2017-12-02 19:04:31 +0300

    Add default and type options to add method

--- spec/alter_table_statement_spec.cr
+++ spec/alter_table_statement_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 
 describe LuckyMigrator::AlterTableStatement do
   it "can alter tables" do
-    statement = LuckyMigrator::AlterTableStatement.new(:users).build do
+    built = LuckyMigrator::AlterTableStatement.new(:users).build do
       add name : String
       add age : Int32
       add completed : Bool
@@ -12,15 +12,41 @@ describe LuckyMigrator::AlterTableStatement do
       remove :old_field
     end
 
-    statement.should eq <<-SQL
+    built.statements.first.should eq <<-SQL
     ALTER TABLE users
       ADD name text NOT NULL,
       ADD age int NOT NULL,
       ADD completed boolean NOT NULL,
-      ADD joined_at timestamp NOT NULL,
+      ADD joined_at timestamptz NOT NULL,
       ADD amount_paid decimal NOT NULL,
       ADD email text,
       DROP old_field
+    SQL
+  end
+
+  it "sets default values" do
+    built = LuckyMigrator::AlterTableStatement.new(:users).build do
+      add name : String, default: "name"
+      add email : String?, default: "optional"
+      add age : Int32, default: 1
+      add num : Int64, default: 1
+      add amount_paid : Float, default: 1.0
+      add completed : Bool, default: false
+      add joined_at : Time, default: :now
+      add future_time : Time, default: Time.new
+    end
+
+    built.statements.size.should eq 1
+    built.statements.first.should eq <<-SQL
+    ALTER TABLE users
+      ADD name text NOT NULL DEFAULT 'name',
+      ADD email text DEFAULT 'optional',
+      ADD age int NOT NULL DEFAULT 1,
+      ADD num bigint NOT NULL DEFAULT 1,
+      ADD amount_paid decimal NOT NULL DEFAULT 1.0,
+      ADD completed boolean NOT NULL DEFAULT false,
+      ADD joined_at timestamptz NOT NULL DEFAULT NOW(),
+      ADD future_time timestamptz NOT NULL DEFAULT '#{Time.new.to_utc}'
     SQL
   end
 end
--- src/lucky_migrator/alter_table_statement.cr
+++ src/lucky_migrator/alter_table_statement.cr
@@ -1,4 +1,12 @@
+require "./*"
+
 class LuckyMigrator::AlterTableStatement
+  include LuckyMigrator::ColumnTypeOptionHelpers
+  include LuckyMigrator::ColumnDefaultHelpers
+
+  alias ColumnType = String.class | Time.class | Int32.class | Int64.class | Bool.class | Float.class
+  alias ColumnDefaultType = String | Time | Int32 | Int64 | Float32 | Float64 | Bool | Symbol
+
   getter statement = IO::Memory.new
   getter rows = [] of String
   getter dropped_rows = [] of String
@@ -7,61 +15,52 @@ class LuckyMigrator::AlterTableStatement
   end
 
   def build
-    statement << "ALTER TABLE #{@table_name}"
-    statement << "\n"
     with self yield
-    process_rows
-    statement.to_s
+    self
   end
 
-  private def process_rows
-    statement << (rows + dropped_rows).join(",\n")
+  def statements
+    [alter_statement]
   end
 
-  macro add(type_declaration)
+  def alter_statement
+    String.build do |statement|
+      statement << "ALTER TABLE #{@table_name}"
+      statement << "\n"
+      statement << (rows + dropped_rows).join(",\n")
+    end
+  end
+
+  macro add(type_declaration, default = nil, **type_options)
+    {% options = type_options.empty? ? nil : type_options %}
+
     {% if type_declaration.type.is_a?(Union) %}
-      add_column :{{ type_declaration.var }}, {{ type_declaration.type.types.first }}, optional: true
+      add_column :{{ type_declaration.var }}, {{ type_declaration.type.types.first }}, optional: true, default: {{ default }}, options: {{ options }}
     {% else %}
-      add_column :{{ type_declaration.var }}, {{ type_declaration.type }}
+      add_column :{{ type_declaration.var }}, {{ type_declaration.type }}, default: {{ default }}, options: {{ options }}
     {% end %}
   end
 
-  def add_column(name : Symbol, type : (Bool | String | Time | Int32 | Int64 | Float).class, optional = false)
+  def add_column(name : Symbol, type : (Bool | String | Time | Int32 | Int64 | Float).class, optional = false, default : ColumnDefaultType? = nil, options : NamedTuple? = nil)
+
+    if options
+      column_type_with_options = column_type(type, **options)
+    else
+      column_type_with_options = column_type(type)
+    end
+
     rows << String.build do |row|
       row << "  ADD "
       row << name.to_s
       row << " "
-      row << column_type(type)
+      row << column_type_with_options
       row << null_fragment(optional)
+      row << default_value(type, default) unless default.nil?
     end
   end
 
   def remove(name : Symbol)
     dropped_rows << "  DROP #{name.to_s}"
-  end
-
-  def column_type(type : String.class)
-    "text"
-  end
-
-  def column_type(type : Time.class)
-    "timestamp"
-  end
-
-  def column_type(type : Int32.class)
-    "int"
-  end
-
-  def column_type(type : Int64.class)
-    "bigint"
-  end
-
-  def column_type(type : Float.class)
-    "decimal"
-  end
-
-  def column_type(type : Bool.class)
-    "boolean"
   end
 
   def null_fragment(optional)
