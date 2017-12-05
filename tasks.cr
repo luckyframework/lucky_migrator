a17dd2fb67b06d4a39fd6a02d8c58ed3396387ca
Paul Smith <paulcsmith0218@gmail.com>
2017-12-04 21:53:30 -0500

    Use DATABASE_URL if present

--- tasks.cr
+++ tasks.cr
@@ -7,10 +7,14 @@ require "./db/migrations/**"
 database = "lucky_migrator_dev"
 
 LuckyRecord::Repo.configure do
-  settings.url = LuckyRecord::PostgresURL.build(
-    hostname: "localhost",
-    database: database
-  )
+  if ENV["DATABASE_URL"]?
+    settings.url = ENV["DATABASE_URL"]
+  else
+    settings.url = LuckyRecord::PostgresURL.build(
+      hostname: "localhost",
+      database: database
+    )
+  end
 end
 
 LuckyMigrator::Runner.configure do
