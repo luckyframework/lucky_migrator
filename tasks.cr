4ddcb4ce5bb4486fe3fe88ec68326000fbd2d331
mikeeus <itsmikias@gmail.com>
2017-12-12 17:17:10 +0300

    rebase

--- tasks.cr
+++ tasks.cr
@@ -3,26 +3,6 @@
 require "lucky_cli"
 require "./src/lucky_migrator"
 require "./db/migrations/**"
-<<<<<<< 1cacfa958cbb9e05a95c11d83ba6cd13db5d9a52
-
-database = "lucky_migrator_dev"
-
-LuckyRecord::Repo.configure do
-  if ENV["DATABASE_URL"]?
-    settings.url = ENV["DATABASE_URL"]
-  else
-    settings.url = LuckyRecord::PostgresURL.build(
-      hostname: "localhost",
-      database: database
-    )
-  end
-end
-
-LuckyMigrator::Runner.configure do
-  settings.database = database
-end
-=======
 require "./config/database"
->>>>>>> Move database and lucky_record configuration to config folder
 
 LuckyCli::Runner.run
