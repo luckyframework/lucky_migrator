71b8766d6a43855f51dce1cbc4b21b3a1f455b88
mikeeus <itsmikias@gmail.com>
2017-12-28 19:13:55 +0300

    Add pending spec

--- spec/migration_spec.cr
+++ spec/migration_spec.cr
@@ -15,6 +15,21 @@ class MigrationThatPartiallyWorks::V999 < LuckyMigrator::Migration::V1
   end
 end
 
+class MigrationWithOrderDependentExecute::V998 < LuckyMigrator::Migration::V1
+  def migrate
+    execute "CREATE TABLE execution_order ();"
+
+    alter :execution_order do
+      add bar : String?
+    end
+
+    execute "ALTER TABLE execution_order ADD new_col text;"
+  end
+
+  def rollback
+  end
+end
+
 describe LuckyMigrator::Migration::V1 do
   it "executes statements in a transaction" do
     expect_raises Exception, %(relation "table_does_not_exist" does not exist) do
@@ -25,5 +40,18 @@ describe LuckyMigrator::Migration::V1 do
       db.query_one? "SELECT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'fake_things');", as: Bool
     end
     exists.should be_false
+  end
+
+  pending "statement execution order" do
+    Spec.after_each do
+      LuckyRecord::Repo.db.exec "DROP TABLE execution_order;"
+    end
+
+    it "runs execute statements in the order they were called" do
+      MigrationWithOrderDependentExecute::V998.new.up
+      columns = LuckyRecord::Repo.table_columns("execution_order")
+      columns.map(&.name).should includes?("new_col")
+      columns.map(&.name).should includes?("bar")
+    end
   end
 end
