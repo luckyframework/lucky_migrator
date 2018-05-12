755819d9010618d20e8b1a2bbca41f9477ad5f46
Paul Smith <paulcsmith0218@gmail.com>
2018-05-12 10:36:56 -0400

    Test custom migration

---
+++ spec/gen/migration_spec.cr
@@ -0,0 +1,16 @@
+require "../spec_helper"
+
+include CleanupHelper
+
+describe Gen::Migration do
+  it "generates a migration with custom name" do
+    with_cleanup do
+      Gen::Migration.new.call("Custom")
+      should_generate_migration named: "custom.cr"
+    end
+  end
+end
+
+private def should_generate_migration(named name : String)
+  Dir.new("./db/migrations").any?(&.ends_with?(name)).should be_true
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,3 +1,4 @@
 require "spec"
+require "./support/**"
 require "../src/lucky_migrator.cr"
 require "../config/database"
