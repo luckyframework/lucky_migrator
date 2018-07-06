db367ca7537428e2d3f06f8ffa0e3fa96061faf1
David Roetzel <david@roetzel.de>
2018-07-06 15:04:35 +0200

    Use `SET NULL` instead of `NULLIFY`.
    According to
    
    https://www.postgresql.org/docs/9.5/static/ddl-constraints.html
    
    this should be the right syntax. See #111

--- src/lucky_migrator/references_helper.cr
+++ src/lucky_migrator/references_helper.cr
@@ -4,8 +4,17 @@ module LuckyMigrator::ReferencesHelper
       ""
     elsif on_delete == :do_nothing
       " REFERENCES #{table_name}"
-    elsif CreateForeignKeyStatement::ALLOWED_ON_DELETE_STRATEGIES.includes?(on_delete)
-      " REFERENCES #{table_name}" + " ON DELETE " + "#{on_delete}".upcase
+    else
+      " REFERENCES #{table_name}" + " ON DELETE " + on_delete_sql(on_delete) 
+    end
+  end
+
+  private def on_delete_sql(on_delete : Symbol?) : String
+    case on_delete
+    when :nullify
+      "SET NULL"
+    when :cascade, :restrict
+      on_delete.to_s.upcase
     else
       raise "on_delete: :#{on_delete} is not supported. Please use :do_nothing, :cascade, :restrict, or :nullify"
     end
